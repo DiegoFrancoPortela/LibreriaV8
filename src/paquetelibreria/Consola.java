e5a1e0960d5b48ce07c43af377d5192cc4324df5
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-02-23 11:48:54 +0100

    Modificacion de Libreria.

--- src/paquetelibreria/Consola.java
+++ src/paquetelibreria/Consola.java
@@ -1,7 +1,7 @@
 package paquetelibreria;
 
 import javax.swing.*;
-
+//
 public class Consola {
     public static String SaludoVentana(String mensajeVentana) {
         JOptionPane.showMessageDialog(null,mensajeVentana);
