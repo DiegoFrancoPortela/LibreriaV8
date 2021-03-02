ea30c3cb0925bf4fec2b511351a635562544eaf0
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-03-02 10:49:07 +0100

    EntradaVentana añadida.

---
+++ src/paquetelibreria/EntradaVentana.java
@@ -0,0 +1,8 @@
+package paquetelibreria;
+
+public class EntradaVentana {
+    public static void main(String[] args) {
+        Ventana vtn = new Ventana();
+        vtn.SaludoVentana("Hola Ventana");
+    }
+}
--- src/paquetelibreria/Ventana.java
+++ src/paquetelibreria/Ventana.java
@@ -1,7 +1,9 @@
 package paquetelibreria;
 
+import javax.swing.*;
+
 public class Ventana {
-    public static void SaludoConsola(String mensajeConsola) {
-        System.out.println(mensajeConsola);
+    public static void SaludoVentana(String mensajeVentana) {
+        JOptionPane.showMessageDialog(null,mensajeVentana);
     }
 }
