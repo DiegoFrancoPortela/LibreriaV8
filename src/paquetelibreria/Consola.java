d776aedd1faa5ee04d6447e4e726c259bee943c0
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-03-02 10:47:23 +0100

    EntradaConsola añadida.

--- src/paquetelibreria/Consola.java
+++ src/paquetelibreria/Consola.java
@@ -1,10 +1,7 @@
 package paquetelibreria;
 
-import javax.swing.*;
-//
 public class Consola {
-    public static String SaludoVentana(String mensajeVentana) {
-        JOptionPane.showMessageDialog(null,mensajeVentana);
-        return mensajeVentana;
+    public static void SaludoConsola(String mensajeConsola) {
+        System.out.println(mensajeConsola);
     }
 }
---
+++ src/paquetelibreria/EntradaConsola.java
@@ -0,0 +1,8 @@
+package paquetelibreria;
+
+public class EntradaConsola {
+    public static void main(String[] args) {
+        Consola cnl = new Consola();
+        cnl.SaludoConsola("Hola Consola");
+    }
+}
